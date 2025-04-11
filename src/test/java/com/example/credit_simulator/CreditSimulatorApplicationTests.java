d71cf34ace2e9bcff111629ef4e0b7de3f72f67e
Jemmi Epriadi <d00028@maucash.id>
2025-04-11 12:25:32 +0700

    add update

---
+++ src/test/java/com/example/credit_simulator/CreditSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.credit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
